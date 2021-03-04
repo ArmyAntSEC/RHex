3eb590a71b05845ba5e6009ad0d01d1e708a7701
Daniel Armyr <daniel.armyr@frescano.se>
2021-03-04 20:38:07 +0100

    Now sends PID parameters as well.

--- Matlab_Arduino_Prototype/VisualizePIDData.m
+++ Matlab_Arduino_Prototype/VisualizePIDData.m
@@ -6,10 +6,11 @@ clc;
 dev = serialport ( 'COM4', 115200 ); 
 dev.configureTerminator( 'CR' );
 pause(2);
-dev.write ( 0, 'uint8' );
+targetSpeed = 5000;
+dev.write ( [0.5 0 2 targetSpeed], 'single' );
 
 %% Load the data
-pause(2);
+pause(3);
 chars = dev.NumBytesAvailable;
 raw = dev.read( chars, 'char' );
 
@@ -20,10 +21,15 @@ data = table ( str2double(D{1,1}), str2double(D{1,2}), 'VariableNames', C{1} );
 data.Time = (1:height(data))'*0.01;
 
 %% Now look at the data
-subplot ( 2, 1, 1 );
-plot ( data.Time, data.Speed, [0 max(data.Time)], 5000*[1 1] );
-subplot ( 2, 1, 2 );
-plot ( data.Time, data.Power );
+subplot ( 3, 1, 1 );
+plot ( data.Time, data.Speed, [0 max(data.Time)], targetSpeed*[1 1] );
+title ( 'Speed' );
+subplot ( 3, 1, 2 );
+plot ( data.Time, data.Power, data.Time(1:end-1), diff(data.Speed)/5 );
+title ( 'Power' );
+subplot ( 3, 1, 3 );
+plot ( data.Time(1:end-2), diff(diff(data.Speed)) );
+title ( 'Jerk' );
 
 clear('dev');
 disp ( 'Done' );
